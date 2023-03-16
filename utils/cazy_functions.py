99a8878502a346a1e21c468dbc2bbe84db65a78e
Chris Field <94464835+crfield18@users.noreply.github.com>
2023-03-16 14:13:14 +0000

    added clean_up function. os swappd for pathlib

--- utils/cazy_functions.py
+++ utils/cazy_functions.py
@@ -1,15 +1,15 @@
-import os
+import pathlib
 import zipfile
 
 # Unzip zipped file
-def unzip(zip_file:str, output_dir:str):
-    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-        zip_ref.extractall(output_dir)
+def unzip(zip_file:pathlib.PosixPath, output_dir:pathlib.PosixPath):
+    with zipfile.ZipFile(str(zip_file), 'r') as zip_ref:
+        zip_ref.extractall(str(output_dir))
 
 # Make directory if it doesn't exist
-def dir_exists(directory:str):
-    if os.path.exists(directory) is False:
-        return os.makedirs(directory)
+def dir_exists(directory:pathlib.PosixPath):
+    if pathlib.Path.is_dir(directory) is False:
+        return pathlib.Path.mkdir(directory)
     return None
 
 # Convert each value in combined_list to an integer if possible
@@ -18,3 +18,8 @@ def num_convert(num_string:str):
         return int(num_string)
     except ValueError:
         return None
+
+# Delete all temp files created by wget
+def clean_up(cwd:pathlib.PosixPath):
+    for file in cwd.glob('**/*.tmp'):
+        pathlib.Path.unlink(file)
