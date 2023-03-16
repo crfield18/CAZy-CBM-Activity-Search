b91fb87bcc09c914d3e1576e67ee71803b192f32
Chris Field <94464835+crfield18@users.noreply.github.com>
2023-03-16 16:51:04 +0000

    separated 'done' statements with new lines

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ def main():
 
     # Download all CBM pages
     parse.wget_CAZy()
-    print('wget_cazy() done')
+    print('\nwget_cazy() done\n')
 
     # Convert/combine html tables into an excel file
     parse.html_to_excel()
-    print('html_to_excel() done')
+    print('\nhtml_to_excel() done\n')
 
     # Download cazy_data.zip
     db_zip = trim.wget_database()
-    print('wget_database() done')
+    print('\nwget_database() done\n')
 
     # Extract cazy_data.zip
     func.unzip(zip_file=db_zip, output_dir=downloads_dir)
-    print('unzip() done')
+    print('\nunzip() done\n')
 
     # Search for cazy_data.txt in the downloads folder
     for file in downloads_dir.glob('**/cazy_data.txt'):
@@ -35,11 +35,11 @@ def main():
         # Read the database file and extract the lines relevant to CBMs
             trim.db_trim(file)
             break
-    print('db_trim() done')
+    print('\ndb_trim() done\n')
 
     # Delete any .tmp files created by python3-wget
     func.clean_up(cwd)
-    print('clean_up() done\n\nCAZy Database Parser complete!')
+    print('\nclean_up() done\n\nCAZy Database Parser complete!\n')
 
 if __name__ == '__main__':
     main()
